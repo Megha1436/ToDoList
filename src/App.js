a5d7b11a06ebb5905823957da7269ad75b8fd73d
Megha1436 <meghadutta201@gmail.com>
2019-09-14 12:07:21 +0530

    comment  added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,241 @@
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {currentTime :new Date().toLocaleTimeString()  }
+  state = {ToDoArray :[]}
+  deleteItem = (index) => {
+    let copy =[...this.state.ToDoArray];
+    copy.splice(index,1);
+    this.setState({ToDoArray : copy})
+  }
+  addToItem = (text) =>{
+    let newarr = [... this.state.ToDoArray,text]
+    this.setState({ToDoArray : newarr});
   } 
-    
-   changeTime = () =>{
-     this.setState({currentTime :new Date().toLocaleTimeString() });
-   }
-   
-   
-   render(){
-     return(
-       <div>
-       <p>
-       {this.state.currentTime}
-       </p>
-       </div>
-     );
-   }
-    
-    
-  componentDidMount(){
-    setInterval(this.changeTime,1000);
+  
+  render(){
+    return(
+      <div>
+      <ToDoInput addItem ={this.addToItem}/>
+      <ToDoList item={this.state.ToDoArray} delItem = {this.deleteItem} />
+        </div>
+    ); 
   }
 }
+
+
+class ToDoInput extends React.Component{
+  state = {inputVal : ""}
+  handleChange = (event) =>{
+    this.setState({inputVal : event.target.value})
+  }
+  handleClick = () => {
+    this.props.addItem(this.state.inputVal);
+ this.setState({inputVal : ""})
+  }
+  render(){
+    return(
+     <div>
+     <input value={this.state.inputVal} onChange = {this.handleChange}/>
+     <button onClick = {this.handleClick}>add
+      </button>
+      </div>
+    );
+  }
+}
+
+
+
+class ToDoList extends React.Component{
+  render(){
+    let total = this.props.item.map((items,index) =>     <li>
+    {items}
+    <button onClick = { () => this.props.delItem(index)}>Delete
+    </button>
+    </li>)
+   return(
+       <div>
+       {total}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));class App extends React.Component{
+  state = {ToDoArray :[]}
+  deleteItem = (index) => {
+    let copy =[...this.state.ToDoArray];
+    copy.splice(index,1);
+    this.setState({ToDoArray : copy})
+  }
+  addToItem = (text) =>{
+    let newarr = [... this.state.ToDoArray,text]
+    this.setState({ToDoArray : newarr});
+  } 
+  
+  render(){
+    return(
+      <div>
+      <ToDoInput addItem ={this.addToItem}/>
+      <ToDoList item={this.state.ToDoArray} delItem = {this.deleteItem} />
+        </div>
+    ); 
+  }
+}
+
+
+class ToDoInput extends React.Component{
+  state = {inputVal : ""}
+  handleChange = (event) =>{
+    this.setState({inputVal : event.target.value})
+  }
+  handleClick = () => {
+    this.props.addItem(this.state.inputVal);
+ this.setState({inputVal : ""})
+  }
+  render(){
+    return(
+     <div>
+     <input value={this.state.inputVal} onChange = {this.handleChange}/>
+     <button onClick = {this.handleClick}>add
+      </button>
+      </div>
+    );
+  }
+}
+
+
+
+class ToDoList extends React.Component{
+  render(){
+    let total = this.props.item.map((items,index) =>     <li>
+    {items}
+    <button onClick = { () => this.props.delItem(index)}>Delete
+    </button>
+    </li>)
+   return(
+       <div>
+       {total}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));class App extends React.Component{
+  state = {ToDoArray :[]}
+  deleteItem = (index) => {
+    let copy =[...this.state.ToDoArray];
+    copy.splice(index,1);
+    this.setState({ToDoArray : copy})
+  }
+  addToItem = (text) =>{
+    let newarr = [... this.state.ToDoArray,text]
+    this.setState({ToDoArray : newarr});
+  } 
+  
+  render(){
+    return(
+      <div>
+      <ToDoInput addItem ={this.addToItem}/>
+      <ToDoList item={this.state.ToDoArray} delItem = {this.deleteItem} />
+        </div>
+    ); 
+  }
+}
+
+
+class ToDoInput extends React.Component{
+  state = {inputVal : ""}
+  handleChange = (event) =>{
+    this.setState({inputVal : event.target.value})
+  }
+  handleClick = () => {
+    this.props.addItem(this.state.inputVal);
+ this.setState({inputVal : ""})
+  }
+  render(){
+    return(
+     <div>
+     <input value={this.state.inputVal} onChange = {this.handleChange}/>
+     <button onClick = {this.handleClick}>add
+      </button>
+      </div>
+    );
+  }
+}
+
+
+
+class ToDoList extends React.Component{
+  render(){
+    let total = this.props.item.map((items,index) =>     <li>
+    {items}
+    <button onClick = { () => this.props.delItem(index)}>Delete
+    </button>
+    </li>)
+   return(
+       <div>
+       {total}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));class App extends React.Component{
+  state = {ToDoArray :[]}
+  deleteItem = (index) => {
+    let copy =[...this.state.ToDoArray];
+    copy.splice(index,1);
+    this.setState({ToDoArray : copy})
+  }
+  addToItem = (text) =>{
+    let newarr = [... this.state.ToDoArray,text]
+    this.setState({ToDoArray : newarr});
+  } 
+  
+  render(){
+    return(
+      <div>
+      <ToDoInput addItem ={this.addToItem}/>
+      <ToDoList item={this.state.ToDoArray} delItem = {this.deleteItem} />
+        </div>
+    ); 
+  }
+}
+
+
+class ToDoInput extends React.Component{
+  state = {inputVal : ""}
+  handleChange = (event) =>{
+    this.setState({inputVal : event.target.value})
+  }
+  handleClick = () => {
+    this.props.addItem(this.state.inputVal);
+ this.setState({inputVal : ""})
+  }
+  render(){
+    return(
+     <div>
+     <input value={this.state.inputVal} onChange = {this.handleChange}/>
+     <button onClick = {this.handleClick}>add
+      </button>
+      </div>
+    );
+  }
+}
+
+
+
+class ToDoList extends React.Component{
+  render(){
+    let total = this.props.item.map((items,index) =>     <li>
+    {items}
+    <button onClick = { () => this.props.delItem(index)}>Delete
+    </button>
+    </li>)
+   return(
+       <div>
+       {total}
+      </div>
+    );
+  }
+}//jfue
+
 ReactDOM.render(<App/>, document.getElementById("root"));
